2e8289947c6d5d0f60caec8a27bcd53b92809eb3
Diego <77411511+DiegoViana90@users.noreply.github.com>
2024-05-09 00:35:26 -0300

    feat: adiciona primeira migration e configuração do startUp

---
+++ Migrations/20240509032407_addCreationDate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ServiceSystem.Migrations
+{
+    public partial class addCreationDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
