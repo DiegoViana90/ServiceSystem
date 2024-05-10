3bc2e685e0e5d1398707d89552ad99673286bea2
Diego Viana <dtviana90@gmail.com>
2024-05-10 04:33:18 -0300

    build: teste

---
+++ wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿function openTab(evt, tabName) {
+    evt.preventDefault()
+    debugger
+    var i, tabContent, tabLinks;
+    tabContent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+    tabLinks = document.getElementsByClassName("nav-link");
+    for (i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
